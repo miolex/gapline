39c76dead3a906b70e4a322ef07e47f7a27c0932
miolexxx <mirchuko@mydigicode.com>
2023-06-10 01:35:58 +0300

    fix gp assets

--- gp.config.ts
+++ gp.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, ConfigEnv, UserConfigExport } from 'vite';
 
 export default function ({}: ConfigEnv): UserConfigExport {
     return defineConfig({
+        base: "https://miolex.github.io/gapline",
         build: {
             outDir: "docs",
             assetsDir: "./"
