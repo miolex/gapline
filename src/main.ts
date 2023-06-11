cf902c507d861efe67e2a6791e03663d707f3db9
miolexxx <mirchuko@mydigicode.com>
2023-06-11 17:57:15 +0300

    build prod

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
 import './style.css';
-import {dragElement} from "./utils/drag.utils";
 import {App} from "./app/app";
 
 window.addEventListener("load", main);
 
 function main() {
     new App();
-}
-
-function setUpPanels() {
-    dragElement(document.getElementById("panel-container")!);
-    dragElement(document.getElementById("settings-panel-container")!);
-    document.getElementById("settings-panel-container")!.style.top = "547px";
 }
